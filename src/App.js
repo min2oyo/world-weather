e995ac2d6f653efb2ceaf97eb00a9ea6e2b04224
min2oyo <min2u@outlook.com>
2022-06-08 15:19:47 +0900

    버튼 보여주기

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import WeatherButton from './component/WeatherButton';
 function App() {
 
   const [weather, setWeather] = useState(null);
+  const cities = [`paris`, `new york`, `tokyo`, `seoul`];
 
   useEffect(() => {
     getCurrentLocation();
@@ -39,7 +40,7 @@ function App() {
     <div>
       <div className='container'>
         <WeatherBox weather={weather} />
-        <WeatherButton />
+        <WeatherButton cities={cities} />
       </div>
     </div>
   );
