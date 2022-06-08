00de2ea6bf1b3d29324ef28d663adab5b25296b8
min2oyo <min2u@outlook.com>
2022-06-08 15:10:57 +0900

    날씨 데이터 보여주기

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@
 // 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다
 // 데이터를 들고오는 동안 로딩 스피너가 돈다
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
 
 function App() {
+
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    getCurrentLocation();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -22,21 +29,16 @@ function App() {
   };
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${`93976fd635a5ca9cfda6c66184d6d3fe`}`;
-    let reponse = await fetch(url);
-    let data = await reponse.json();
-    console.log(`data`, data);
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${`93976fd635a5ca9cfda6c66184d6d3fe&units=metric`}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
   };
-
-  useEffect(() => {
-    getCurrentLocation();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <div>
       <div className='container'>
-        <WeatherBox />
+        <WeatherBox weather={weather} />
         <WeatherButton />
       </div>
     </div>
--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const WeatherBox = () => {
+const WeatherBox = ({ weather }) => {
+
   return (
     <div className='weather-box'>
-      <div>서울</div>
-      <h2>30도 / 섭씨 230화씨</h2>
-      <h3>맑은 하늘</h3>
+      <div>{weather?.name}</div>
+      <h2>{weather?.main.temp}C / {weather?.main.temp * 1.8 + 32}화씨</h2>
+      <h3>{weather?.weather[0].description}</h3>
     </div>
   );
 };
