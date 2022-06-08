ce02a2699a3bbb79a04bb054a9114fdf88abc8fd
min2oyo <min2u@outlook.com>
2022-06-08 14:07:02 +0900

    현재위치 기반 날씨 가져오기

--- src/App.js
+++ src/App.js
@@ -14,8 +14,16 @@ function App() {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
-      console.log(`현재위치`, lat, lon);
+      getWeatherByCurrentLocation(lat, lon);
     });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${`93976fd635a5ca9cfda6c66184d6d3fe`}`;
+    let reponse = await fetch(url);
+    let data = await reponse.json();
+    console.log(`data`, data);
+
   };
 
   useEffect(() => {
@@ -24,7 +32,6 @@ function App() {
 
   return (
     <div>
-      {console.log(`ㅋ`)}
       hi
     </div>
   );
